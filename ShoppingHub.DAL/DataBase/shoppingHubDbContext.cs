3537ecc88aa1bf0fad26a43a2ba4b666b49fce6d
Mohamed Talaat <mot141414@gmail.com>
2025-09-14 17:23:58 +0300

    add ef core and init migration

--- ShoppingHub.DAL/DataBase/shoppingHubDbContext.cs
+++ ShoppingHub.DAL/DataBase/shoppingHubDbContext.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ShoppingHub.DAL.DataBase
 {
-    internal class shoppingHubDbContext
+    internal class shoppingHubDbContext:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=ShoppingHubDb;Trusted_Connection=True;TrustServerCertificate=True");
+        }
     }
 }
