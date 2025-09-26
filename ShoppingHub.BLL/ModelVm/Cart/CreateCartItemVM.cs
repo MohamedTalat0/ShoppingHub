542e95993570dd00b2cbe8f4addd71e5cca9bae0
Mohamed Talaat <mot141414@gmail.com>
2025-09-26 16:17:12 +0300

    merge main wuth develop

---
+++ ShoppingHub.BLL/ModelVm/Cart/CreateCartItemVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingHub.BLL.ModelVm.Cart
+{
+    public class CreateCartItemVM
+    {
+        public string UserID { get; set; }
+        public int ProductID { get; set; }
+        public int Quantity { get; set; }
+    }
+}
